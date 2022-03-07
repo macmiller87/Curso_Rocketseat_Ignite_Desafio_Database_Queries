eb7934b805691bd9b69df189c78a013d0249512d
Macmiller <macamagolf@gmail.com>
2022-03-07 20:59:07 -0300

    Logica e queries com typeorm tabela users

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,26 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: {
+        id: user_id
+      } 
+    });
+    
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name ASC`); 
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE LOWER(first_name) ILIKE $1 AND LOWER(last_name) ILIKE $2`, [first_name, last_name]); 
   }
 }
